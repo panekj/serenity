7b99731509fd39da0b672bcd11d1147dd9dd28fe
Gnome! <david2005thomas@Gmail.com>
2024-01-06 00:14:25 +0000

    Rewrite builders to take `Cow`s (#2688)

--- src/model/channel/message.rs
+++ src/model/channel/message.rs
@@ -1,5 +1,6 @@
 //! Models relating to Discord channels.
 
+use std::borrow::Cow;
 #[cfg(feature = "model")]
 use std::fmt::Display;
 #[cfg(all(feature = "cache", feature = "model"))]
@@ -396,7 +397,11 @@ impl Message {
     /// Returns a [`ModelError::MessageTooLong`] if the message contents are too long.
     ///
     /// [Manage Messages]: Permissions::MANAGE_MESSAGES
-    pub async fn edit(&mut self, cache_http: impl CacheHttp, builder: EditMessage) -> Result<()> {
+    pub async fn edit(
+        &mut self,
+        cache_http: impl CacheHttp,
+        builder: EditMessage<'_>,
+    ) -> Result<()> {
         if let Some(flags) = self.flags {
             if flags.contains(MessageFlags::IS_VOICE_MESSAGE) {
                 return Err(Error::Model(ModelError::CannotEditVoiceMessage));
@@ -679,7 +684,7 @@ impl Message {
     pub async fn reply(
         &self,
         cache_http: impl CacheHttp,
-        content: impl Into<String>,
+        content: impl Into<Cow<'_, str>>,
     ) -> Result<Message> {
         self.reply_(cache_http, content, Some(false)).await
     }
@@ -703,7 +708,7 @@ impl Message {
     pub async fn reply_ping(
         &self,
         cache_http: impl CacheHttp,
-        content: impl Into<String>,
+        content: impl Into<Cow<'_, str>>,
     ) -> Result<Message> {
         self.reply_(cache_http, content, Some(true)).await
     }
@@ -739,7 +744,7 @@ impl Message {
     async fn reply_(
         &self,
         cache_http: impl CacheHttp,
-        content: impl Into<String>,
+        content: impl Into<Cow<'_, str>>,
         inlined: Option<bool>,
     ) -> Result<Message> {
         #[cfg(feature = "cache")]
