a28b331fca08208dd778254f70b8d2b0d9fbce39
Gnome! <david2005thomas@Gmail.com>
2024-03-29 20:13:19 +0000

    Add audit log reasons to remaining model methods (#2821)

--- src/model/channel/message.rs
+++ src/model/channel/message.rs
@@ -268,7 +268,7 @@ impl Message {
     /// current user does not have the required permissions.
     ///
     /// [Manage Messages]: Permissions::MANAGE_MESSAGES
-    pub async fn delete(&self, cache_http: impl CacheHttp) -> Result<()> {
+    pub async fn delete(&self, cache_http: impl CacheHttp, reason: Option<&str>) -> Result<()> {
         #[cfg(feature = "cache")]
         {
             if let (Some(cache), Some(guild_id)) = (cache_http.cache(), self.guild_id) {
@@ -283,7 +283,7 @@ impl Message {
             }
         }
 
-        self.channel_id.delete_message(cache_http.http(), self.id).await
+        self.channel_id.delete_message(cache_http.http(), self.id, reason).await
     }
 
     /// Deletes all of the [`Reaction`]s associated with the message.
@@ -524,7 +524,7 @@ impl Message {
     /// does not have the required permissions.
     ///
     /// [Manage Messages]: Permissions::MANAGE_MESSAGES
-    pub async fn pin(&self, cache_http: impl CacheHttp) -> Result<()> {
+    pub async fn pin(&self, cache_http: impl CacheHttp, reason: Option<&str>) -> Result<()> {
         #[cfg(feature = "cache")]
         {
             if let (Some(cache), Some(guild_id)) = (cache_http.cache(), self.guild_id) {
@@ -537,7 +537,7 @@ impl Message {
             }
         }
 
-        self.channel_id.pin(cache_http.http(), self.id).await
+        self.channel_id.pin(cache_http.http(), self.id, reason).await
     }
 
     /// React to the message with a custom [`Emoji`] or unicode character.
@@ -784,7 +784,7 @@ impl Message {
     /// does not have the required permissions.
     ///
     /// [Manage Messages]: Permissions::MANAGE_MESSAGES
-    pub async fn unpin(&self, cache_http: impl CacheHttp) -> Result<()> {
+    pub async fn unpin(&self, cache_http: impl CacheHttp, reason: Option<&str>) -> Result<()> {
         #[cfg(feature = "cache")]
         {
             if let (Some(cache), Some(guild_id)) = (cache_http.cache(), self.guild_id) {
@@ -797,7 +797,7 @@ impl Message {
             }
         }
 
-        cache_http.http().unpin_message(self.channel_id, self.id, None).await
+        cache_http.http().unpin_message(self.channel_id, self.id, reason).await
     }
 
     /// Ends the [`Poll`] on this message, if there is one.
